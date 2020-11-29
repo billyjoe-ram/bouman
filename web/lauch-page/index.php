df0c0466718dcdcbc1bb2537431892d175b26b99
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-11-29 15:04:42 -0300

    Style Adjustments

--- web/lauch-page/index.php
+++ web/lauch-page/index.php
@@ -9,22 +9,22 @@ $texto = "Formulário de pré-cadastro";
     <div class="row">
         <div class="col-md-4"></div>
         <div class="col-md-6">
-            <h1 id="texto"> <?php echo $texto?></h1>
+            <center><h1> <?php echo $texto?></h1></center>
         </div>
         <div class="col-md-2"></div>
     </div>
     <div class="row">
         <div class="col-md-1"></div>
         <div class="col-md-4" id="texto">
-            <h3 id="destaque">O QUE É O BOUMAN?</h3>
-            <p>O Bouman é a mais nova <span id="destaque">plataforma</span> pensada e adaptada para os <span id="destaque">cientistas e inventores brasileiros</span>, oferecendo diversas
-            ferramentas que <span id="destaque">facilitam o desenvolvimento</span> de pesquisas e o trabalho em grupo.</p>
-            <p>Pensando no senso de comunidade, oferecemos a opção dos <span id="destaque">usuários contribuirem</span> em trabalhos de outros pesquisadores, 
-            oferecendo sugestões e análises.  Também, visando o investimento nestes projetos, temos uma interface amigável e <span id="destaque">compatível 
+            <h3 class="destaque">O QUE É O BOUMAN?</h3>
+            <p>O Bouman é a mais nova <span class="destaque">plataforma</span> pensada e adaptada para os <span class="destaque">cientistas e inventores brasileiros</span>, oferecendo diversas
+            ferramentas que <span class="destaque">facilitam o desenvolvimento</span> de pesquisas e o trabalho em grupo.</p>
+            <p>Pensando no senso de comunidade, oferecemos a opção dos <span class="destaque">usuários contribuirem</span> em trabalhos de outros pesquisadores, 
+            oferecendo sugestões e análises.  Também, visando o investimento nestes projetos, temos uma interface amigável e <span class="destaque">compatível 
             com empresas</span> que buscam novos talentos e querem se conectar com os profissionais.</p>
-            <p>Infelzmente não podemos fazer isso, <span id="destaque">sem a sua ajuda</span>! Nossos usuários são nossa maior prioridade e queremos que eles 
+            <p>Infelzmente não podemos fazer isso, <span class="destaque">sem a sua ajuda</span>! Nossos usuários são nossa maior prioridade e queremos que eles 
             conheçam melhor o que oferecemos e estejam preparados para quando finalmente nos lançarmos na internet! Preencha o formulário 
-            ao lado se você tem interesse de receber nossos <span id="destaque">Termos de Uso</span> previamente e ser um dos primeiros a explorar a plataforma!</p>
+            ao lado se você tem interesse de receber nossos <span class="destaque">Termos de Uso</span> previamente e ser um dos primeiros a explorar a plataforma!</p>
         </div>
         <div class="col-md-1"></div>
         <div class="col-md-5">
