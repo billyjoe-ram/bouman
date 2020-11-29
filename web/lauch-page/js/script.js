b0e29aa8c91d50bee1a7b7ed9a7e0d2d583a8f1b
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-11-29 17:28:03 -0300

    js code added

---
+++ web/lauch-page/js/script.js
@@ -0,0 +1,45 @@
+console.log("working;");
+
+const mascara = {
+    // Máscara para celular
+    celu(value) {        
+        return value
+        // Impedir valores não-numéricos
+        .replace(/\D/g, '')
+        // Adicionar parênteses de DDD
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        // Adicionar hífen
+        .replace(/(\d{5})(\d)/, '$1-$2')
+        // Limitar valores
+        .replace(/(-\d{4})\d+?$/, '$1')
+    },
+
+    // Máscara para telefone
+    tele(value) {        
+        return value
+        // Impedir valores não-numéricos
+        .replace(/\D/g, '')
+        // Adicionar parênteses de DDD
+        .replace(/(\d{2})(\d)/, '($1) $2')
+        // Adicionar hífen
+        .replace(/(\d{4})(\d)/, '$1-$2')
+        // Limitar valores
+        .replace(/(-\d{4})\d+?$/, '$1')
+    }
+
+}
+
+// Obtendo dados dos inputs
+document.querySelectorAll('input').forEach(($input) => {
+    const campo = $input.dataset.js    
+    $input.addEventListener('input', (e) => {
+        e.target = mascara[campo](e.target.value);
+    }, false)
+
+})
+
+function texto(){
+    //mudança do texto h1 para "Pré-cadastro realizado com sucesso!"
+
+    document.getElementById("texto").innerHTML = "Pré-cadastro realizado com sucesso!";    
+}
