277344d8f736da93522190a7ee7c732299190bb8
Billy Joe Ramos <billyjoersantos@outlook.com>
2020-12-07 16:25:29 -0300

    Data receiving page

--- web/lauch-page/cadastrorealizado.php
+++ web/lauch-page/cadastrorealizado.php
@@ -5,6 +5,18 @@
     include_once 'include/rules/verificacao.php';
 ?>
 <body>
- ola mundo ;)   
+    <!-- CABEÇALHO -->    
+    <header>        
+        <span id="titulo">Projeto Bouman</span>
+        <img src="imagem/logo.png" id="logo">
+    </header>    
+    <h1 class="titulo-pag"> Cadastro realizado com sucesso! </h1>
+    <!--FIM CABEÇALHO -->
+
+    <main>
+        <h3>Entraremos em contato em breve avisando de mais informações</h3>
+
+        <img src="assets/undraw_creative_experiment_8dk3.svg" alt="Experimento criativo" class="vector-mid">
+    </main>
 </body>
 </html>
