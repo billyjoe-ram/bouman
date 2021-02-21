a2106b65d8bf082430ba47981036b8cfd9d0757f
Billy Joe Ramos <billyjoersantos@outlook.com>
2021-02-21 17:43:23 -0300

    routing and navigating

--- web/ng-bouman/src/app/app-routing.module.ts
+++ web/ng-bouman/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './components/feed/feed.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { ConfigurateComponent } from './components/signup/configurate/configurate.component';
+import { InfosComponent } from './components/signup/infos/infos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: "signup", pathMatch: 'full' },
+  { path: 'feed', component: FeedComponent },
+  { path: 'signup', component: InfosComponent, children: [
+    { path: 'config',  component: ConfigurateComponent }
+  ]},
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfilePageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
