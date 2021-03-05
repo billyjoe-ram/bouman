e8c175139434aff39808f157e356b713d04be069
Billy Joe Ramos <billyjoersantos@outlook.com>
2021-03-04 23:07:58 -0300

    changing images in the profile (must be saved)

--- ng-bouman/src/app/components/profile-page/profile-card/profile-card.component.ts
+++ ng-bouman/src/app/components/profile-page/profile-card/profile-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/storage';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'profile-card',
@@ -8,14 +9,39 @@ import { AngularFireStorage } from '@angular/fire/storage';
 })
 export class ProfileCardComponent implements OnInit {
 
-  constructor(private storage: AngularFireStorage) { }
+  private imgPath: string = "";
+  public profileImg: any = "/assets/profile-example.png";
+  public wallpImg: any = "/assets/wallpaper-example.jpg";
+
+  constructor(private storage: AngularFireStorage, private auth: AuthService) { }
     
   ngOnInit(): void {
   }
-  uploadFile(event:any) {
-    const file = event.target.files[0];
-    console.log(file)
-    /*const filePath = 'name-your-file-path-here';
-    const task = this.storage.upload(filePath, file);*/
+
+  async upload($event: any) {
+    this.imgPath = $event.target.files[0];
   }
+
+  async profilePic() {
+    const user = await this.auth.getAuth().currentUser;
+
+    const filePath = `profile-pictures/${user?.uid}`;
+    const ref = this.storage.upload(filePath, this.imgPath);
+    const fileRef = this.storage.ref(filePath);
+    fileRef.getDownloadURL().subscribe(url => {
+      this.profileImg = url;
+    });    
+  }
+
+  async wallpaPic() {
+    const user = await this.auth.getAuth().currentUser;
+        
+    const filePath = `wallpaper-pictures/${user?.uid}`;
+    const ref = this.storage.upload(filePath, this.imgPath);
+    const fileRef = this.storage.ref(filePath);
+    fileRef.getDownloadURL().subscribe(url => {
+      this.wallpImg = url;
+    });    
+  }
+
 }
