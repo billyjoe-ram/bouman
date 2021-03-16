c2c9d122a6aee31376130b00e42a7f4cc832dd18
Denis-Holanda <67616367+Denis-Holanda@users.noreply.github.com>
2021-03-16 18:58:00 -0300

    Revert "TRYING TO FIX THE DAMN THING"
    This reverts commit 33caf0a4cb83fb959603c0bec1e9da5b684692ca.

--- src/app/components/projects/project-edit/project-edit.component.ts
+++ src/app/components/projects/project-edit/project-edit.component.ts
@@ -1,51 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { toBase64String } from '@angular/compiler/src/output/source_map';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Project } from 'src/app/interfaces/project';
-import { AuthService } from 'src/app/services/auth.service';
-import { DocsService } from 'src/app/services/docs.service';
-
 @Component({
   selector: 'app-project-edit',
   templateUrl: './project-edit.component.html',
-  styleUrls: ['./project-edit.component.css'],
+  styleUrls: ['./project-edit.component.css']
 })
 export class ProjectEditComponent implements OnInit {
 
-  public docForm!: FormGroup;
-
-  // private docServ: DocsService, 
-
-  constructor(private auth: AuthService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.docForm = new FormGroup({
-      title: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(1),
-      ]),
-      content: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(1),
-      ]),
-    });
   }
 
-  async onSubmit() {
-    const user = await this.auth.getAuth().currentUser;
-    const submitted = this.docForm.value;
-    const date = new Date();
-    const project: Project = {
-      ownerId: user?.uid,
-      docId: toBase64String(date.toString()),
-      title: submitted.title,
-      content: submitted.content,
-      createdAt: date,
-    };
-
-    console.log('Informações do projeto: ' + project);
-
-    // this.docServ.addProject(project);
-  }
 }
--- src/app/components/projects/project-overview/project-overview.component.ts
+++ src/app/components/projects/project-overview/project-overview.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DocsService } from 'src/app/services/docs.service';
 
 @Component({
   selector: 'project-overview',
@@ -8,14 +7,9 @@ import { DocsService } from 'src/app/services/docs.service';
 })
 export class ProjectOverviewComponent implements OnInit {
 
-  public projects: any[] = [];
-  
-  constructor(private docServ: DocsService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.projects = this.docServ.listProjects();
-
-    console.log(this.projects);
   }
 
 }
