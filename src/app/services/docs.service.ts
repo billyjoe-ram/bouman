e935739db6b22f86072386c7cdc7762bf1031f42
Billy Joe Ramos <billyjoersantos@outlook.com>
2021-03-24 09:41:29 -0300

    tá tudo meio cagado

--- src/app/services/docs.service.ts
+++ src/app/services/docs.service.ts
@@ -10,7 +10,7 @@ import { UsersService } from './users.service';
 })
 export class DocsService {
 
-  private docsCollection = this.store.collection<Project>('Projects');
+  private docsCollection = this.store.collection<Project>('Users/Projects');
   private projects: any[] = [];
 
   constructor(private store: AngularFirestore, private auth: AuthService) { }
@@ -27,8 +27,35 @@ export class DocsService {
     return query;
   }
   
-  addProject(project: Project) {
-    return this.docsCollection.add(project);
+  async addProject(project: Project) {
+    const owner = await this.auth.getAuth().currentUser;
+    /*
+    try {
+        const newUser = await this.authService.register(this.userRegister);        
+
+        await this.store
+          .collection('Users')
+          .doc(newUser.user?.uid)
+          .set(userObject);
+
+        userObject.id = newUser.user?.uid;
+
+        await this.store
+          .collection('Users')
+          .doc(newUser.user?.uid)
+          .update(userObject);
+    */
+    const newDoc = this.docsCollection.doc(owner?.uid).set(project);
+    
+    const projectRef = this.docsCollection.doc(owner?.uid);
+
+    projectRef.get().subscribe(data => {
+      project.docId = data.id;
+    });
+
+    console.log(project);
+    
+    projectRef.update(project);
   }
 
 }
