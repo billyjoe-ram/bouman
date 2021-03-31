3860d332ad92e135305f414df4b152ceb9c694be
Billy Joe Ramos <billyjoersantos@outlook.com>
2021-03-31 20:28:00 -0300

    Added the ngx-bootstrap module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { LoginComponent } from './components/login/login.component';
 
 import { DropdownDirectiveDirective } from './directives/dropdown-directive.directive';
 import { AngularEditorModule } from '@kolkov/angular-editor';
-import { EditorModule } from '@tinymce/tinymce-angular';
+
 import { ProjectsComponent } from './components/projects/projects.component';
 import { ProjectOverviewComponent } from './components/projects/project-overview/project-overview.component';
 import { ProjectComponent } from './components/projects/project-overview/project/project.component';
@@ -60,7 +60,6 @@ import { environment } from 'src/environments/environment.prod';
     AngularFireAuthModule,
     AngularFirestoreModule,
     HttpClientModule,
-    EditorModule,
     AngularEditorModule
   ],
   providers: [],
