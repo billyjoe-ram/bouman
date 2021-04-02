f4bd8dd6fa844ec3df2b0050f50012fb21388e8e
Pedro Reinaldo <66150760+PedroReinaldoBastos@users.noreply.github.com>
2021-04-02 00:47:06 -0300

    tá pela metade
    por enquanto tá funcionando, mas bugou no meu PC, então estou joker porque não está funcionando.

--- src/app/components/signup/infos/infos.component.ts
+++ src/app/components/signup/infos/infos.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewChecked } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AreasService } from 'src/app/services/areas.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'sign-infos',
@@ -15,8 +17,11 @@ export class InfosComponent implements OnInit {
 
   public userRegister: any = {};
   public areas: any = {};
+  emailverified : boolean = false;
+  teste !: Subscription;
 
   constructor(
+    private firebase: AngularFireAuth,
     private authService: AuthService,
     private router: Router,
     private store: AngularFirestore,
@@ -25,9 +30,11 @@ export class InfosComponent implements OnInit {
 
   ngOnInit(): void {
     this.areas = this.areasService.getAreas();
+    
   }
 
-  async signup() {            
+  async signup() {        
+
     if (this.form.valid) {
       this.userRegister = {
         name: this.form.value.name,
@@ -39,29 +46,48 @@ export class InfosComponent implements OnInit {
       const userObject = Object.assign({}, this.userRegister);      
 
       delete userObject.email;
-      delete userObject.password;      
-
+      delete userObject.password;       
+               
       try {
-        const newUser = await this.authService.register(this.userRegister);        
+      const newUser = await this.authService.register(this.userRegister);        
+  
+      await this.store
+        .collection('Users')
+        .doc(newUser.user?.uid)
+        .set(userObject);
+  
+      userObject.id = newUser.user?.uid;
+  
+      await this.store
+        .collection('Users')
+        .doc(newUser.user?.uid)
+        .update(userObject);
 
-        await this.store
-          .collection('Users')
-          .doc(newUser.user?.uid)
-          .set(userObject);
-
-        userObject.id = newUser.user?.uid;
-
-        await this.store
-          .collection('Users')
-          .doc(newUser.user?.uid)
-          .update(userObject);
-
-      } catch (error) {
-        console.error(error);
-      } finally {
-        this.router.navigate(['/config']);
-      }
+      const user = await this.authService.getAuth().currentUser;
+      if (user != null){
+      user.sendEmailVerification().then(function (){
+        // Email enviado corretamente.
+        user.providerData.forEach(function (profile:any) {
+        console.log("Sign-in provider: " + profile.providerId);
+        console.log("  Provider-specific UID: " + profile.uid);
+        console.log("  Name: " + profile.displayName);
+        console.log("  Email: " + profile.email);
+        console.log("  Photo URL: " + profile.photoURL)
+        console.log("  E-mail verified?: " + profile.verified);
+        })
+    }).catch(function(error){
+      // Um erro ocorreu.
+        console.log('um erro ocorreu');
+        user.reload().then(function(){
+        console.log("  E-mail verified22222: " + user.emailVerified);})
+    })
+    }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.router.navigate(['/profile']);
     }
   }
-
 }
+}
+
