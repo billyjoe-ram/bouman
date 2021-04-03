fde2ad888cafcabba8921f9b2324a39f7a7eb715
Billy Joe Santos <billyjoersantos@outlook.com>
2021-04-02 22:15:18 -0300

    Configurate component now only shows after e-mail verification

--- src/app/components/signup/configurate/configurate.component.ts
+++ src/app/components/signup/configurate/configurate.component.ts
@@ -12,98 +12,51 @@ import { IbgeService } from 'src/app/services/ibge.service';
 })
 export class ConfigurateComponent implements OnInit {
   @ViewChild('boumanForm') form!: NgForm;
-
-  public genders: string[] = ['Masculino', 'Feminino'];
+  
   public states: { id: number, sigla: string }[] = [];  
   public cities: { id: number, nome: string }[] = [];  
-  emailverified : any;
+  
+  private emailverified!: any;
   
   constructor(
     private authService: AuthService,
     private store: AngularFirestore,
-    private router: Router,
-    private ibgeService: IbgeService
+    private ibgeService: IbgeService,
+    private router: Router
   ) {}
 
   ngOnInit(): void {
     this.getStates();
-    this.config();
   }
 
-  async config() {
+  async onSign() {
+    const user = await this.authService.getAuth().currentUser;
 
-      const user = await this.authService.getAuth().currentUser;
-      await user?.reload();
-      this.emailverified = user?.emailVerified; 
-      if (this.emailverified == undefined || this.emailverified == false){
-        if (user != null){
-        user.sendEmailVerification().then(function (){
-          // Email enviado corretamente.
-          user.providerData.forEach(function (profile:any) {
-            window.alert("Foi enviado um link de verificação de usuário para o seu E-mail, por favor na sua caixa de entrada: "+profile.email)
-          })
-      }).catch(function(error){
-        // Um erro ocorreu.
-          window.alert('um erro ocorreu, verifique se na sua caixa de entrada já não possui um link de verificação, caso não haja, tente novamente...');
-      })
-      }
-    }else if(this.emailverified == true){
+    this.emailverified = user?.emailVerified;
 
-      if (this.form.valid) {
+    const verified = this.emailverified;
+
+    if (this.form.valid && verified && user) {
 
       const description = this.form.value.desc;
       const date = this.form.value.date;
       const state = this.form.value.state;
       const city = this.form.value.city;
+
       try {
         await this.store
           .collection('Users')
           .doc(user?.uid)
           .update({ desc: description, birth: date, state: state, city: city });
-        
-          window.alert('E-mail foi verificado, você será redirecionado para pagína do seu perfil.');
-          this.router.navigate(['/profile'])
+
+          this.router.navigate(["/feed"]);
+
       } catch (error) {
         console.error(error);
       }
-    }    
-    else{
-      console.log("Ocorreu um erro no formulário.");
-      window.alert("Ocorreu um erro no formulário.");
     }
-  }
-    // this.validaData();
-  }
 
-  validaData(){
-    var data = this.form.value.date;
-    var dataAtual = new Date();
-    var nascimento = new Date(data)
-    var ano = dataAtual.getFullYear() - nascimento.getFullYear();
- 
-    if(ano == 18){
-      if(dataAtual.getMonth() == nascimento.getMonth()){
-        if(dataAtual.getDay() <= nascimento.getDay()){
-          //menor de idade
-        }
-        else{
-          //maior de idade
-        }
-      }
-      else if(dataAtual.getMonth() < nascimento.getMonth()){
-        //menor de idade
-      }
-      else{
-        //maior de idade
-      }
-    }
-    else if(ano < 18){
-      //menor de idade
-    }
-    else{
-      //maior de idade
-    }
-  }
+  }  
 
   public getStates() {
     this.ibgeService.getStates().subscribe((res) => {
