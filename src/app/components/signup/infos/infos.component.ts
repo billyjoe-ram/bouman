b64b8f196f180b56109171151cd812541f7a6e81
Billy Joe Santos <billyjoersantos@outlook.com>
2021-04-02 22:18:38 -0300

    infos.components.ts updated

--- src/app/components/signup/infos/infos.component.ts
+++ src/app/components/signup/infos/infos.component.ts
@@ -17,20 +17,24 @@ export class InfosComponent implements OnInit {
   public userRegister: any = {};
   public areas: any = {};
 
+  private emailverified: any;
+
   constructor(
     private authService: AuthService,
     private router: Router,
     private store: AngularFirestore,
     private areasService: AreasService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     this.areas = this.areasService.getAreas();
   }
 
-  async signup() {        
+  async signup() {
+    const user = await this.authService.getAuth().currentUser;
+    
 
-    if (this.form.valid) {
+    if (this.form.valid && !user) {
       this.userRegister = {
         name: this.form.value.name,
         email: this.form.value.email,
@@ -38,31 +42,60 @@ export class InfosComponent implements OnInit {
         password: this.form.value.passkey,
       };
 
-      const userObject = Object.assign({}, this.userRegister);      
+      const userObject = Object.assign({}, this.userRegister);
 
       delete userObject.email;
-      delete userObject.password;       
-               
+      delete userObject.password;
+
       try {
-      const newUser = await this.authService.register(this.userRegister);        
-  
-      await this.store
-        .collection('Users')
-        .doc(newUser.user?.uid)
-        .set(userObject);
-  
-      userObject.id = newUser.user?.uid;
-  
-      await this.store
-        .collection('Users')
-        .doc(newUser.user?.uid)
-        .update(userObject);
-    } catch (error) {
-      console.error(error);
-    } finally {
-              this.router.navigate(['/config']);
+        const newUser = await this.authService.register(this.userRegister);
+
+        await this.store
+          .collection('Users')
+          .doc(newUser.user?.uid)
+          .set(userObject);
+
+        userObject.id = newUser.user?.uid;
+
+        await this.store
+          .collection('Users')
+          .doc(newUser.user?.uid)
+          .update(userObject);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        this.config();
+      }
+
     }
   }
-}
+
+  async config() {
+    const user = await this.authService.getAuth().currentUser;
+
+    this.emailverified = user?.emailVerified;
+
+    if (!this.emailverified && user != null){
+
+      user.sendEmailVerification().then(() => {
+        // Email enviado corretamente.
+        user.providerData.forEach((profile:any) => {
+          window.alert("Foi enviado um link de verificação de usuário para o seu e-mail: " + profile.email)
+        });
+      }).catch((error) => {
+        // Um erro ocorreu.        
+        window.alert('Um erro ocorreu, verifique se na sua caixa de entrada já não possui um link de verificação, caso não haja, tente novamente...');
+
+        console.error(error);
+      }).finally(() => {
+        this.authService.logout();
+        this.form.reset();
+        this.router.navigate(["/login"]);
+      });
+      
+    }
+
+  }
+
 }
 
