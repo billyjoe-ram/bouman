9fc52c6e7baa90eabf2256364bdc20dd086bcadf
Billy Joe R. Santos <billyjoersantos@outlook.com>
2021-05-06 14:20:09 -0300

    Importing module

--- src/app/components/profile-page/contact/contact.component.ts
+++ src/app/components/profile-page/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { AreasService } from 'src/app/services/areas.service';
