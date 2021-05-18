e0abedd0d6a92188bb6f4ad7c5e1a00f692a1580
Billy Joe R. Santos <billyjoersantos@outlook.com>
2021-05-17 21:47:08 -0300

    Using component to display and post projects

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -20,11 +20,7 @@ export class FeedComponent implements OnInit, OnDestroy {
 
   @ViewChild('errorModalTrigger') errorModalTrigger!: ElementRef;
   
-  @ViewChild('projectsModalTrigger') projectsModalTrigger!: ElementRef;
-
-  @ViewChild('selectedProjectContent') selectedProjectContent!: ElementRef;
-
-  @ViewChild('checkBoxes') checkBoxes!: ElementRef;
+  // @ViewChild('projectsModalTrigger') projectsModalTrigger!: ElementRef;
   
   public content: string = "";
 
@@ -35,10 +31,6 @@ export class FeedComponent implements OnInit, OnDestroy {
   public feedPosts: { profileId: string, posts: Post[] }[] = [];
 
   public userProjects: Project[] = [];
-
-  public selectedProject!: any;
-
-  public selectedProjectText: string[] = [];
 
   public errorMsg: string = "";
   
@@ -55,8 +47,7 @@ export class FeedComponent implements OnInit, OnDestroy {
     private posts: PostsService,
     private user: UsersService,
     private profile: ProfileService,
-    private docs: DocsService,
-    private projects: ProjectsService) { }
+    private docs: DocsService) { }
 
   ngOnInit(): void {
     this.loadData();
@@ -130,90 +121,26 @@ export class FeedComponent implements OnInit, OnDestroy {
     });    
   }
 
-  importProject() {
-    // Reset the array
-    this.userProjects = [];
+  // importProject() {
+  //   // Reset the array
+  //   this.userProjects = [];
 
-    this.docs.listProjects().then((projects) => {
-      // Retrieving user projects
-      projects.forEach((project) => {
-        // Add to the array
-        this.userProjects.push(project.data());
-      });
+  //   this.docs.listProjects().then((projects) => {
+  //     // Retrieving user projects
+  //     projects.forEach((project) => {
+  //       // Add to the array
+  //       this.userProjects.push(project.data());
+  //     });
 
-      // Ordering by date
-      this.userProjects.sort((a: any, b: any) => {
-        return a.lastEdit.seconds - b.lastEdit.seconds;
-      }).reverse();
-    });    
+  //     // Ordering by date
+  //     this.userProjects.sort((a: any, b: any) => {
+  //       return a.lastEdit.seconds - b.lastEdit.seconds;
+  //     }).reverse();
+  //   });    
 
-    // Triggering modal
-    this.projectsModalTrigger.nativeElement.click();
-  }
-
-  selectProject(project: Project) {
-    this.selectedProject = project;
-  }
-
-  showProject() {
-    this.addEvents();
-
-    // Ordering keys that were messed
-    const orderedKeys = Object.keys(this.selectedProject.content).sort();
-    
-    // Creating a new Object: ProjectCotent
-    const orderedObject: ProjectContent = { aResum: "", bIntro: "", cObj: "", dMetod: "", eResult: "", fCons: "", gRef: "" };
-    
-    // For each ordered key, in their order, get the value from the same key of projContent
-    orderedKeys.forEach((key) => {
-      orderedObject[key] = this.selectedProject.content[key];
-    });
-
-    // With the copy now ordered and populated, pass it to projContent
-    this.selectedProject.content = orderedObject;
-
-  }
-
-  postProject() {
-    if(this.selectedProjectText.length) {
-      this.projects.addProject(this.profileId, this.selectedProjectText.join(' '));
-    } else {
-      this.handleError("Selecione pelo menos uma parte do projeto...");
-    }
-    
-  }
-
-  loadProjectPart(projPart: string): string {
-    let contentKey = projPart;
-
-    // Loading the project part selected
-    switch (projPart.trim()) {
-      case "Resumo":
-        contentKey = "aResum";
-      break;
-      case "Introdução":
-        contentKey = "bIntro";
-      break;
-      case "Objetivos":
-        contentKey = "cObj";
-      break;
-      case "Metodologia":
-        contentKey = "dMetod";
-      break;
-      case "Resultados":
-        contentKey = "eResult";
-      break;
-      case "Considerações":
-        contentKey = "fCons";
-      break;
-      case "Referências":
-        contentKey = "gRef";
-      break;
-    }
-
-    // Return the value of the key
-    return contentKey;
-  }
+  //   // Triggering modal
+  //   this.projectsModalTrigger.nativeElement.click();
+  // }    
 
   handleError(errorMsg: string) {
     this.errorMsg = errorMsg;
@@ -235,65 +162,6 @@ export class FeedComponent implements OnInit, OnDestroy {
       this.postsSubs.unsubscribe();
     }
 
-  }
-  
-  private addEvents() {
-    // Selecting element
-    const textArea = this.selectedProjectContent.nativeElement;    
-
-    // Checking if the element was created
-    if (this.checkBoxes) {
-      // Getting the element itself
-      const checks = this.checkBoxes.nativeElement;
-      
-      // Getting its children
-      const checkControls = checks.children;
-
-      // For each .form-check, handle 
-      for (let control of checkControls) {
-        // Form check children
-        const controlChildren = control.children;
-
-        // Input itself
-        const checkboxInput = controlChildren[0];
-
-        // Add a callback attached to click
-        checkboxInput.addEventListener("click", (event: any) => {
-          // Clearing content
-          textArea.innerHTML = "";
-
-          // Event target
-          let input = event.target;
-
-          // Input label
-          const checkboxLabel = input.nextElementSibling;
-
-          // Label text
-          const labelText = checkboxLabel.innerHTML;
-
-          // Get selected working part
-          const workingPart = this.loadProjectPart(labelText);
-
-          // Verify checkbox
-          if (input.checked) {
-            // Add content if is checked
-            this.selectedProjectText.push(this.selectedProject.content[workingPart]);
-          } else {
-            // Remove content if it isn't checked
-            this.selectedProjectText = this.selectedProjectText.filter((element) => {
-              return element !== this.selectedProject.content[workingPart];
-            });
-          }
-
-          // Adding text to modal
-          this.selectedProjectText.forEach(projectChapter => {
-            textArea.innerHTML += projectChapter;
-          });
-          
-        });
-      }
-      
-    }
   }
 
 }
---
+++ src/app/components/feed/post-project/post-project.component.ts
@@ -0,0 +1,177 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Project } from 'src/app/interfaces/project';
+import { ProjectContent } from 'src/app/interfaces/projectContent';
+import { DocsService } from 'src/app/services/docs.service';
+import { ProjectsService } from 'src/app/services/projects.service';
+
+@Component({
+  selector: 'post-project',
+  templateUrl: './post-project.component.html',
+  styleUrls: ['./post-project.component.css']
+})
+export class PostProjectComponent implements OnInit {
+
+  @Input('projects') userProjects: Project[] = [];
+
+  @Input('profileId') profileId: string | undefined = "";
+
+  @ViewChild('projectsModalTrigger') projectsModalTrigger!: ElementRef;
+
+  @ViewChild('selectedProjectContent') selectedProjectContent!: ElementRef;
+
+  @ViewChild('checkBoxes') checkBoxes!: ElementRef;  
+
+  public selectedProjectText: string[] = [];
+
+  public selectedProject!: any;
+  
+  constructor(private projectsService: ProjectsService, private docsService: DocsService) { }  
+
+  ngOnInit(): void {
+  }
+
+  importProject() {
+    // Reset the array
+    this.userProjects = [];
+
+    this.docsService.listProjects().then((projects) => {
+      // Retrieving user projects
+      projects.forEach((project) => {
+        // Add to the array
+        this.userProjects.push(project.data());
+      });
+
+      // Ordering by date
+      this.userProjects.sort((a: any, b: any) => {
+        return a.lastEdit.seconds - b.lastEdit.seconds;
+      }).reverse();
+    });    
+
+    // Triggering modal
+    this.projectsModalTrigger.nativeElement.click();
+  }
+
+  selectProject(project: Project) {
+    this.selectedProject = project;
+  }
+
+  showProject() {
+    this.addEvents();
+
+    // Ordering keys that were messed
+    const orderedKeys = Object.keys(this.selectedProject.content).sort();
+    
+    // Creating a new Object: ProjectCotent
+    const orderedObject: ProjectContent = { aResum: "", bIntro: "", cObj: "", dMetod: "", eResult: "", fCons: "", gRef: "" };
+    
+    // For each ordered key, in their order, get the value from the same key of projContent
+    orderedKeys.forEach((key) => {
+      orderedObject[key] = this.selectedProject.content[key];
+    });
+
+    // With the copy now ordered and populated, pass it to projContent
+    this.selectedProject.content = orderedObject;
+
+  }
+
+  loadProjectPart(projPart: string): string {
+    let contentKey = projPart;
+
+    // Loading the project part selected
+    switch (projPart.trim()) {
+      case "Resumo":
+        contentKey = "aResum";
+      break;
+      case "Introdução":
+        contentKey = "bIntro";
+      break;
+      case "Objetivos":
+        contentKey = "cObj";
+      break;
+      case "Metodologia":
+        contentKey = "dMetod";
+      break;
+      case "Resultados":
+        contentKey = "eResult";
+      break;
+      case "Considerações":
+        contentKey = "fCons";
+      break;
+      case "Referências":
+        contentKey = "gRef";
+      break;
+    }
+
+    // Return the value of the key
+    return contentKey;
+  }
+
+  postProject() {
+    if(this.selectedProjectText.length) {
+      this.projectsService.addProject(this.profileId, this.selectedProjectText.join(' '));
+    } else {
+      alert("Selecione pelo menos uma parte do projeto...");
+    }
+    
+  }
+  
+  private addEvents() {
+    // Selecting element
+    const textArea = this.selectedProjectContent.nativeElement;    
+
+    // Checking if the element was created
+    if (this.checkBoxes) {
+      // Getting the element itself
+      const checks = this.checkBoxes.nativeElement;
+      
+      // Getting its children
+      const checkControls = checks.children;
+
+      // For each .form-check, handle 
+      for (let control of checkControls) {
+        // Form check children
+        const controlChildren = control.children;
+
+        // Input itself
+        const checkboxInput = controlChildren[0];
+
+        // Add a callback attached to click
+        checkboxInput.addEventListener("click", (event: any) => {
+          // Clearing content
+          textArea.innerHTML = "";
+
+          // Event target
+          let input = event.target;
+
+          // Input label
+          const checkboxLabel = input.nextElementSibling;
+
+          // Label text
+          const labelText = checkboxLabel.innerHTML;
+
+          // Get selected working part
+          const workingPart = this.loadProjectPart(labelText);
+
+          // Verify checkbox
+          if (input.checked) {
+            // Add content if is checked
+            this.selectedProjectText.push(this.selectedProject.content[workingPart]);
+          } else {
+            // Remove content if it isn't checked
+            this.selectedProjectText = this.selectedProjectText.filter((element) => {
+              return element !== this.selectedProject.content[workingPart];
+            });
+          }
+
+          // Adding text to modal
+          this.selectedProjectText.forEach(projectChapter => {
+            textArea.innerHTML += projectChapter;
+          });
+          
+        });
+      }
+      
+    }
+  }
+
+}
